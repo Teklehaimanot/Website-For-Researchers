667e302b4d07c8b956f7644ebfac3562a94a7297
teklehaimanot <teklehaimanotbelete@gmail.com>
2024-10-16 09:28:40 +0300

    making home section responsiveness

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -16,17 +16,17 @@ const Home = ({ isDarkMode, profile, Interests, educations }) => {
       }`}
     >
       {profile && (
-        <div className="flex flex-row justify-between w-3/4 mx-auto space-x-5  my-14">
-          <div className="w-2/5 flex flex-col space-y-7 ">
+        <div className="flex lg:flex-row flex-col justify-between xl:w-3/4 mx-auto space-x-5  my-14">
+          <div className="lg:w-2/5 flex flex-col space-y-7 ">
             <div className=" space-y-2 flex flex-col items-center">
               <img
                 src={api + "/" + profile.profile_pic?.url}
                 alt="profile preview"
-                className=" w-[17vw] h-[17vw] rounded-full object-cover mx-auto"
+                className=" lg:w-[17vw] lg:h-[17vw] md:w-[30vw] md:h-[30vw] sm:w-[40vw] sm:h-[40vw] w-[60vw] h-[60vw] rounded-full object-cover mx-auto"
               />
               <h2 className=" text-4xl font-light ">{profile.name}</h2>
               <h3 className="text-lg text-gray-500">{profile.position}</h3>
-              <h3 className=" text font-light hover:text-blue-700   ">
+              <h3 className=" font-light hover:text-blue-700 ">
                 <a href="https://smartcsvtool.com" target="blank">
                   {profile.Institution}
                 </a>
@@ -71,7 +71,7 @@ const Home = ({ isDarkMode, profile, Interests, educations }) => {
               </li>
             </ul>
           </div>
-          <div className="w-3/5">
+          <div className="lg:w-3/5 ">
             <div>
               <p className=" font-serif text-xl my-5 text-pretty leading-relaxed tracking-wide whitespace-pre-line ">
                 {profile.description}
