afa8433ba3c9ff8db14b351aae6c830bad3be1ad
teklehaimanot <teklehaimanotbelete@gmail.com>
2024-10-24 15:03:10 +0300

    final commit

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -4,7 +4,7 @@ import { MdEmail } from "react-icons/md";
 import { FaGoogleScholar } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 import { FaGraduationCap } from "react-icons/fa";
-import axios from "axios";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Home = ({ isDarkMode, profile, Interests, educations }) => {
   const api = process.env.REACT_APP_API_URL;
@@ -17,24 +17,34 @@ const Home = ({ isDarkMode, profile, Interests, educations }) => {
     >
       {profile && (
         <div className="flex lg:flex-row flex-col justify-between xl:w-3/4 mx-auto space-x-5  my-14">
-          <div className="lg:w-2/5 flex flex-col space-y-7 ">
-            <div className=" space-y-2 flex flex-col items-center">
-              <img
-                src={api + "/" + profile.profile_pic?.url}
-                alt="profile preview"
-                className=" lg:w-[17vw] lg:h-[17vw] md:w-[30vw] md:h-[30vw] sm:w-[30vw] sm:h-[30vw] w-[50vw] h-[50vw] rounded-full object-cover mx-auto"
-              />
-              <h2 className=" text-4xl font-light ">{profile.name}</h2>
-              <h3 className="text-lg text-gray-500">{profile.position}</h3>
-              <h3 className=" font-light hover:text-blue-700 ">
-                <a href="https://smartcsvtool.com" target="blank">
-                  {profile.Institution}
-                </a>
-              </h3>
+          <div className="lg:w-2/5 flex flex-col space-y-5 ">
+            <div className=" space-y-2 flex flex-col items-center ">
+              <div>
+                <img
+                  src={api + "/" + profile.profile_pic?.url}
+                  alt="profile preview"
+                  className=" lg:w-[17vw] lg:h-[17vw] md:w-[30vw] md:h-[30vw] sm:w-[30vw] sm:h-[30vw] w-[50vw] h-[50vw] rounded-full object-cover mx-auto"
+                />
+              </div>
+              <div className=" flex text-center">
+                <h2 className=" text-4xl font-light ">{profile.name}</h2>
+              </div>
+              <div className=" flex text-center">
+                <h3 className="text-lg text-gray-500  mx-auto">
+                  {profile.position}
+                </h3>
+              </div>
+              <div className=" flex text-center">
+                <h3 className=" font-light hover:text-blue-700 ">
+                  <a href={profile.Institution_link} target="blank">
+                    {profile.Institution}
+                  </a>
+                </h3>
+              </div>
             </div>
             <ul className=" flex  mx-auto space-x-7 flex-wrap ">
               <li className="">
-                <a href={profile.social_medias[1]?.link} target="blank">
+                <a href={profile.social_medias[0]?.link} target="blank">
                   <MdEmail
                     size={32}
                     color={isDarkMode ? "#bbdefb" : "0e76a8"}
@@ -43,7 +53,7 @@ const Home = ({ isDarkMode, profile, Interests, educations }) => {
                 </a>
               </li>
               <li>
-                <a href={profile.social_medias[2]?.link} target="blank">
+                <a href={profile.social_medias[1]?.link} target="blank">
                   <FaGoogleScholar
                     size={32}
                     color={isDarkMode ? "#bbdefb" : "0e76a8"}
@@ -52,7 +62,7 @@ const Home = ({ isDarkMode, profile, Interests, educations }) => {
                 </a>
               </li>
               <li>
-                <a href={profile.social_medias[0]?.link} target="blank">
+                <a href={profile.social_medias[3]?.link} target="blank">
                   <FaGithub
                     size={32}
                     color={isDarkMode ? "#bbdefb" : "0e76a8"}
@@ -61,8 +71,17 @@ const Home = ({ isDarkMode, profile, Interests, educations }) => {
                 </a>
               </li>
               <li>
-                <a href={profile.social_medias[3]?.link} target="blank">
+                <a href={profile.social_medias[2]?.link} target="blank">
                   <FaLinkedin
+                    size={32}
+                    color={isDarkMode ? "#bbdefb" : "0e76a8"}
+                    className="transition-transform transform hover:scale-150"
+                  />
+                </a>
+              </li>
+              <li>
+                <a href={profile.social_medias[4]?.link} target="blank">
+                  <FaXTwitter
                     size={32}
                     color={isDarkMode ? "#bbdefb" : "0e76a8"}
                     className="transition-transform transform hover:scale-150"
@@ -73,14 +92,14 @@ const Home = ({ isDarkMode, profile, Interests, educations }) => {
           </div>
           <div className="lg:w-3/5 ">
             <div>
-              <p className=" font-serif md:text-xl text-md my-5 text-pretty leading-relaxed tracking-wide whitespace-pre-line ">
+              <p className="  text-sm my-5 text-pretty leading-relaxed tracking-wide whitespace-pre-line ">
                 {profile.description}
               </p>
             </div>
             <div className="flex  flex-col md:flex-row md:space-x-10 ">
               <div className="md:w-1/2">
                 <h3 className=" text-2xl font-serif">Interests</h3>
-                <ul className=" ml-7 my-2  font-serif  leading-relaxed tracking-wide list-disc list-inside space-y-2">
+                <ul className=" ml-7 my-2  font-serif text-sm  leading-relaxed tracking-wide list-disc list-inside space-y-2">
                   {Interests?.map((interest) => (
                     <li key={interest.id}>{interest.title}</li>
                   ))}
@@ -88,7 +107,7 @@ const Home = ({ isDarkMode, profile, Interests, educations }) => {
               </div>
               <div className="md:w-1/2">
                 <h3 className=" text-2xl font-serif">Education</h3>
-                <ul className=" ml-7 my-2  font-serif  leading-relaxed tracking-wide space-y-3">
+                <ul className=" ml-7 my-2 text-sm font-serif  leading-relaxed tracking-wide space-y-3">
                   {educations?.map((education) => (
                     <li
                       className=" flex flex-row space-x-4 "
@@ -97,7 +116,7 @@ const Home = ({ isDarkMode, profile, Interests, educations }) => {
                       <FaGraduationCap size={24} />
                       <div className="font-serif  leading-relaxed tracking-wide">
                         <p>
-                          {education.level} in {education.field_of_study},{" "}
+                          {education.field_of_study},{" "}
                           {education.year?.split("-")[0]}
                         </p>
                         <p className=" font-light  text-sm text-gray-500">
